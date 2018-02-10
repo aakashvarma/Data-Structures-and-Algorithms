1db8f68f36c7d85893da5dc8dc539e0358ede792
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-10 23:30:51 +0530

    leap year added

--- leapyear.c
+++ leapyear.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-
+int main(){
+	int N, R;
+	scanf ("%d", &N);
+	if ( N >= 1 && N <= 1000000){
+		R = N % 4;
+		if ( R == 0 ){
+			printf("YES");
+			
+		}
+		else{
+			printf("NO");
+			
+		}
+	}
+	return 0;
+}
 
 
 
